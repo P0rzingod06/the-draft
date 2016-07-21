cb65071132c364cbfcc53d2a25b593f7f2791788
Michael Gassert <michael.gassert@wwt.com>
2016-07-21 15:53:56 -0500

    getting basic files and hello world set up

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'main'
+	template: `<div><h1>{{pageTitle}}</h1>
+        <div>Hello World</div>
+    </div>`
+})
+
+export class AppComponent {
+	pageTitle: String = 'Welcome to The Draft';
+}
---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import {boostrap} from 'angular2/platform/browser';
+
+import {AppComponent} from './app.component';
+
+boostrap{AppComponent};
