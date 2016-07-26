066b1297204ba1686ccfd5d6013e65d09fd0938b
Dale Race <race.dale@gmail.com>
2016-07-26 12:14:17 -0500

    Fix for "can't find module" typescript errors in Atom

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from 'angular2/core';
 import 'rxjs/Rx';
 import { ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
 
-import { DashboardComponent } from 'app/dashboard/dashboard.component';
-import { DraftComponent } from 'app/draft/draft.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DraftComponent } from './draft/draft.component';
 
 @Component({
 	selector: 'td-app',
