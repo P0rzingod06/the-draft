6a647834ac0163717efb570a097562d7c1b64a26
grahaman <andrew.graham@wwt.com>
2016-07-29 13:09:24 -0500

    added data to the component and not hard coded into the html. dont really like the format of it right now, but its an upgrade

--- app/draft/draft.component.ts
+++ app/draft/draft.component.ts
@@ -6,5 +6,30 @@ import {Component} from 'angular2/core';
 })
 
 export class DraftComponent {
+	rounds: any[] = [
+		{
+			"draftRound": 1,
+			"drafter": "Andrew",
+			"draftPick": "Jennifer Aniston",
+			"age": 45
+		},
+		{
+			"draftRound": 1,
+			"drafter": "David",
+			"draftPick": "J Law",
+			"age": 28
+		},
+		{
+			"draftRound": 1,
+			"drafter": "Mike",
+			"draftPick": "Emma Stone",
+			"age": 27
+		},
+		{
+			"draftRound": 1,
+			"drafter": "Dale",
+			"draftPick": "Lively",
+			"age": 28
+		}]
 
 }
