272e5f5f30809cba60ba2da0b8e069c94321eb13
grahaman <andrew.graham@wwt.com>
2016-08-01 09:03:09 -0500

    changed data model to have each manager contain their own picks. much easier to display picks per manager this way.

--- app/draft/draft.component.ts
+++ app/draft/draft.component.ts
@@ -7,70 +7,106 @@ import {Component} from 'angular2/core';
 
 export class DraftComponent {
 
-	rounds: any[] = [
-		{
-			"draftRound": 1,
-			"drafter": "Andrew",
-			"draftPick": "Jennifer Aniston",
-			"age": 45
-		},
-		{
-			"draftRound": 1,
-			"drafter": "David",
-			"draftPick": "J Law",
-			"age": 28
-		},
-		{
-			"draftRound": 1,
-			"drafter": "Mike",
-			"draftPick": "Emma Stone",
-			"age": 27
-		},
-		{
-			"draftRound": 1,
-			"drafter": "Dale",
-			"draftPick": "Lively",
-			"age": 28
-		},
-		{
-			"draftRound": 2,
-			"drafter": "Dale",
-			"draftPick": "Kristen Bell",
-			"age": 45
-		},
-		{
-			"draftRound": 2,
-			"drafter": "Mike",
-			"draftPick": "Emma Watson",
-			"age": 28
-		},
-		{
-			"draftRound": 2,
-			"drafter": "David",
-			"draftPick": "Margot Robbie",
-			"age": 27
-		},
-		{
-			"draftRound": 2,
-			"drafter": "Andrew",
-			"draftPick": "Jessica Alba",
-			"age": 28
-		}];
+	//rounds: any[] = [
+	//	{
+	//		"draftRound": 1,
+	//		"drafter": "Andrew",
+	//		"draftPick": "Jennifer Aniston",
+	//		"age": 45
+	//	},
+	//	{
+	//		"draftRound": 1,
+	//		"drafter": "David",
+	//		"draftPick": "J Law",
+	//		"age": 28
+	//	},
+	//	{
+	//		"draftRound": 1,
+	//		"drafter": "Mike",
+	//		"draftPick": "Emma Stone",
+	//		"age": 27
+	//	},
+	//	{
+	//		"draftRound": 1,
+	//		"drafter": "Dale",
+	//		"draftPick": "Lively",
+	//		"age": 28
+	//	},
+	//	{
+	//		"draftRound": 2,
+	//		"drafter": "Dale",
+	//		"draftPick": "Kristen Bell",
+	//		"age": 45
+	//	},
+	//	{
+	//		"draftRound": 2,
+	//		"drafter": "Mike",
+	//		"draftPick": "Emma Watson",
+	//		"age": 28
+	//	},
+	//	{
+	//		"draftRound": 2,
+	//		"drafter": "David",
+	//		"draftPick": "Margot Robbie",
+	//		"age": 27
+	//	},
+	//	{
+	//		"draftRound": 2,
+	//		"drafter": "Andrew",
+	//		"draftPick": "Jessica Alba",
+	//		"age": 28
+	//	}];
 
 	managers: any[] = [
 		{
-			"Name": "Andrew"
-		},
+			"name": "Andrew",
+			"picks": [{
+				"draftRound": 1,
+				"draftPick": "Jennifer Aniston",
+				"age": 45
+			},
+			{
+				"draftRound": 2,
+				"draftPick": "Jessica Alba",
+				"age": 28
+			}]},
 		{
-			"Name": "Jake"
-		},
+			"name": "Jake",
+			"picks": [ {
+				"draftRound": 1,
+				"draftPick": "J Law",
+				"age": 28
+			},
+			{
+				"draftRound": 2,
+				"draftPick": "Margot Robbie",
+				"age": 27
+			}]},
 		{
-			"Name": "Mike"
-		},
+			"name": "Mike",
+			"picks": [
+				{
+					"draftRound": 1,
+					"draftPick": "Emma Stone",
+					"age": 27
+				},
+				{
+					"draftRound": 2,
+					"draftPick": "Emma Watson",
+					"age": 28
+				}]},
 		{
-			"Name": "Dale"
-		}
-
+			"name": "Dale",
+			"picks": [{
+				"draftRound": 1,
+				"draftPick": "Lively",
+				"age": 28
+			},
+			{
+				"draftRound": 2,
+				"draftPick": "Kristen Bell",
+				"age": 45
+			}]}
 	];
 
 }
