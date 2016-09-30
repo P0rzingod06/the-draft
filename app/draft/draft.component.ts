6242818e1e528c1dfdf3e2bc68867627202b0758
grahaman <andrew.graham@wwt.com>
2016-09-30 09:28:09 -0500

    changes i made a while ago

--- app/draft/draft.component.ts
+++ app/draft/draft.component.ts
@@ -7,56 +7,6 @@ import {Component} from 'angular2/core';
 
 export class DraftComponent {
 
-	//rounds: any[] = [
-	//	{
-	//		"draftRound": 1,
-	//		"drafter": "Andrew",
-	//		"draftPick": "Jennifer Aniston",
-	//		"age": 45
-	//	},
-	//	{
-	//		"draftRound": 1,
-	//		"drafter": "David",
-	//		"draftPick": "J Law",
-	//		"age": 28
-	//	},
-	//	{
-	//		"draftRound": 1,
-	//		"drafter": "Mike",
-	//		"draftPick": "Emma Stone",
-	//		"age": 27
-	//	},
-	//	{
-	//		"draftRound": 1,
-	//		"drafter": "Dale",
-	//		"draftPick": "Lively",
-	//		"age": 28
-	//	},
-	//	{
-	//		"draftRound": 2,
-	//		"drafter": "Dale",
-	//		"draftPick": "Kristen Bell",
-	//		"age": 45
-	//	},
-	//	{
-	//		"draftRound": 2,
-	//		"drafter": "Mike",
-	//		"draftPick": "Emma Watson",
-	//		"age": 28
-	//	},
-	//	{
-	//		"draftRound": 2,
-	//		"drafter": "David",
-	//		"draftPick": "Margot Robbie",
-	//		"age": 27
-	//	},
-	//	{
-	//		"draftRound": 2,
-	//		"drafter": "Andrew",
-	//		"draftPick": "Jessica Alba",
-	//		"age": 28
-	//	}];
-
 	managers: any[] = [
 		{
 			"name": "Andrew",
